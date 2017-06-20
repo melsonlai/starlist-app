b2769c2abbdc8aaed4214a9d1e625f832eb88652
Ming-Yi Lai <s104062201@m104.nthu.edu.tw>
2017-06-20 16:25:24 +0800

    ADD: todo redux action "listTodos"

--- src/states/todo-actions.js
+++ src/states/todo-actions.js
@@ -1,3 +1,5 @@
+import {listTodos as listTodosFromApi} from "../api/todos.js";
+
 /*  Todo item */
 
 export function toggleTooltip(id) {
@@ -12,5 +14,44 @@ export function setTooltipToggle(id, toggle) {
         type: '@TODO_ITEM/SET_TOOLTIP_TOGGLE',
         id,
         toggle
+    };
+};
+
+/* Todo List */
+
+function startListTodos() {
+	return {
+		type: "@TODO_LIST/START_LIST_TODOS"
+	};
+}
+
+function endListTodos(todos) {
+	return {
+		type: "@TODO_LIST/END_LIST_TODOS",
+		todos
+	};
+}
+
+export function listTodos(unaccomplishedOnly, searchText) {
+    return (dispatch, getState) => {
+        dispatch(startListTodos());
+        return listTodosFromApi(unaccomplishedOnly, searchText).then(todos => {
+            dispatch(endListTodos(todos));
+        }).catch(err => {
+            dispatch(endListTodos());
+            console.error('Error listing todos', err);
+        });
+    };
+};
+
+export function listMorePosts(searchText, start) {
+    return (dispatch, getState) => {
+        dispatch(startListMorePosts(start));
+        return listPostsFromApi(searchText, start).then(posts => {
+            dispatch(endListMorePosts(posts));
+        }).catch(err => {
+            dispatch(endListMorePosts());
+            console.error('Error listing more posts', err);
+        });
     };
 };
